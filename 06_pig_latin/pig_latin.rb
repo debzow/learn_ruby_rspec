69fbb0c4b094d0309a6d37178c144244254148a1
debzow <david.gerard.42@gmail.com>
2018-10-08 23:09:25 +0200

    exercice 06

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
-#write your code here
+
+
+def translate(word)
+    #can(/[aeiou]/i)
+    f=word[0]
+    if (f == "a" || f == "e" || f == "i" || f == "o" || f == "u")
+        word = word+"ay"
+    else       
+        first_char = word[0]
+
+        word_tmp = word.byteslice(1, word.length-1) 
+        word_tmp << first_char 
+        word_tmp = word_tmp+"ay"
+
+
+        #last_char = word[word.length-1]
+        #word[0] à déplacer à la fin
+        #rajouter "ay" à la fin
+    end
+end
